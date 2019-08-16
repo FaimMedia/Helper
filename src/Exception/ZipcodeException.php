54514dc0fa573939cf6f35fe48bf3e620f580c0b
Tim Peters (FaimMedia B.V.) <tim@faimmedia.nl>
2019-08-16 14:34:03 +0200

    Added Zipcode helper class

---
+++ src/Exception/ZipcodeException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FaimMedia\Helper\Exception;
+
+use Exception;
+
+/**
+ * Zipcode Exception class
+ */
+class ZipcodeException extends Exception {
+	const EMPTY_CHARACTER = -1;
+}
